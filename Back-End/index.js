9db7d5d9730bd2ed69e6b6d3989872fb3c448e2e
Keval <kevalmehta885@gmail.com>
2024-04-16 20:18:10 +0530

    first

---
+++ Back-End/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express;
+app.use(express.json());
+app.post("/todo", (req, res) => {});
+app.get("/todos", (req, res) => {});
+app.put("/completed", (req, res) => {});
+app.delte("/", (req, res) => {});
