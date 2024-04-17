f0d93ff40d528544581efb55fc96dfafc3fe034f
Keval <kevalmehta885@gmail.com>
2024-04-17 19:33:16 +0530

    testing

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { CreateTodo } from "./components/CreateTodo.jsx";
+import { Todos } from "./components/Todos.jsx";
 import "./App.css";
+import { json } from "express";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  fetch("http://localhost:1000/todos").then(async (res) => {
+    const json = await res.json();
+    setTodos(json.todos);
+  });
   return (
     <div>
       <CreateTodo></CreateTodo>
-      <Todos></Todos>
+      <Todos todos={[{}]}></Todos>
     </div>
   );
 }
