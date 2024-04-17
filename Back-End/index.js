032b1ea4185b2c2d7368ccca2102b6285736a905
Keval <kevalmehta885@gmail.com>
2024-04-17 11:03:35 +0530

    Modified post and put endpoint

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const { createTodo, updateTod } = require("./types");
 const app = express;
 app.use(express.json());
-app.post("/todo", (req, res) => {});
+app.post("/todo", (req, res) => {
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "You sent the Wrong Input",
+    });
+    return;
+  }
+});
 app.get("/todos", (req, res) => {});
-app.put("/completed", (req, res) => {});
+app.put("/completed", (req, res) => {
+  const updatePayload = req.body;
+  const parsedPayload = updateTod.safeParse(updatePayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "You Updated Wrong Inputs",
+    });
+    return;
+  }
+});
 app.delte("/", (req, res) => {});
